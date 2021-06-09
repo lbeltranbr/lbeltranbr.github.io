d0e913f8e8ce425d38600e043f3f6a10d0452cbb
lbeltranbr <57176574+lbeltranbr@users.noreply.github.com>
2021-06-10 00:49:02 +0100

    projects added

---
+++ scripts/projects_effects.js
@@ -0,0 +1,25 @@
+//code from https://www.w3schools.com/howto/howto_js_slideshow.asp
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+  
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("carousel");
+	if (n > slides.length) {slideIndex = 1}    
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";  
+	}
+	slides[slideIndex-1].style.display = "block";  
+	s//etTimeout(showSlides, 4000); // Change image every 2 seconds
+
+  }
